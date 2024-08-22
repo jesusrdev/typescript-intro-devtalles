0a23cc2cddda532415b372912de72bd4797c460f
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-08-22 16:50:36 -0500

    basic types and general concepts

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,9 @@
+const name: string = "Strider";
+let hpPoints: number | "FULL" = 85;
+const isAlive: boolean = true;
+
+hpPoints = "FULL";
+
+console.log({ name, hpPoints, isAlive });
+
+// export {};
