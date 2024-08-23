247b93a96e07e087130c15924e93c405c7b9ad3c
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-08-23 15:50:48 -0500

    functions with objects like params

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -14,8 +14,32 @@ function multiply(
   return firstNumber * base;
 }
 
-const result = addNumbers(1, 2);
-const result2 = addNumbersArrow(1, 2);
-const multyplyResult = multiply(5);
+// const result = addNumbers(1, 2);
+// const result2 = addNumbersArrow(1, 2);
+// const multyplyResult = multiply(5);
 
-console.log({ result, result2, multyplyResult });
+// console.log({ result, result2, multyplyResult });
+
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+  character.hp += amount;
+};
+
+const strider: Character = {
+  name: "Strider",
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de vida ${this.hp}`);
+  },
+};
+
+healCharacter(strider, 10);
+healCharacter(strider, 20);
+
+
+strider.showHp();
