124f2d0ed28e5269793f5cb6e9b3719d88ec26f8
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-08 18:22:43 -0500

    composition over inheritance

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -12,16 +12,31 @@ export class Person {
   constructor(public name: string, private address: string = "No address") {}
 }
 
-export class Hero extends Person {
+// export class Hero extends Person {
+//   constructor(
+//     public alterEgo: string,
+//     public age: number,
+//     public realName: string
+//   ) {
+//     super(realName, "New York");
+//   }
+// }
+
+export class Hero {
+  // public person: Person;
+
   constructor(
     public alterEgo: string,
     public age: number,
-    public realName: string
+    public realName: string,
+    public person: Person,
   ) {
-    super(realName, "New York");
+    // this.person = new Person(realName);
   }
 }
 
-const ironman = new Hero("Ironman", 45, "Tony");
+const tony = new Person("Tony Stark", "New York");
+
+const ironman = new Hero("Ironman", 45, "Tony", tony);
 
 console.log(ironman);
