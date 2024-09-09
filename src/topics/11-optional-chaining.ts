1e6124fb899c1cffefa0723796a6fc64889f5f47
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-08 19:04:19 -0500

    optional chaining

---
+++ src/topics/11-optional-chaining.ts
@@ -0,0 +1,25 @@
+export interface Passenger {
+  name: string;
+  children?: string[];
+}
+
+const passenger1: Passenger = {
+  name: 'Fernando',
+}
+
+const passenger2: Passenger = {
+  name: 'Melissa',
+  children: ['Natalia', 'Elizabeth']
+}
+
+const returnChildrenNumber = (passenger: Passenger): number => {
+
+  const howManyChildren = passenger.children?.length || 0;
+  // const howManyChildren = passenger.children!.length; // el ! es para asegurar que siempre le voy a pasar esa prop
+
+  console.log(passenger.name, howManyChildren);
+
+  return howManyChildren;
+}
+
+returnChildrenNumber(passenger1);
